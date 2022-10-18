c92e63f6671cace77ceae53eb2b967723be84b97
chanho99 <chanho0526@naver.com>
2022-10-18 22:54:13 +0900

    PROJECT221012

---
+++ src/Practice_2.java
@@ -0,0 +1,42 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Practice_2 {
+    public static void main(String[] args){
+        ArrayList<Character> stack = new ArrayList<Character>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("6개의 학점을 빈 칸으로 분리 입력(A/B/C/D/F) >> ");
+
+        int sum = 0;
+        for(int i=0; i<6; i++) {
+            char c = sc.next().charAt(0);
+            stack.add(c);
+        }
+
+        for(int i=0; i<stack.size(); i++) {
+            char c = stack.get(i);
+            switch(c) {
+                case 'A':
+                    sum+=4;
+                    break;
+                case 'B':
+                    sum+=3;
+                    break;
+                case 'C':
+                    sum+=2;
+                    break;
+                case 'D':
+                    sum+=1;
+                    break;
+                case 'F':
+                    sum+=0;
+                    break;
+            }
+        }
+
+        double avg = (double)sum / stack.size();
+        System.out.println(avg);
+        sc.close();
+    }
+}
